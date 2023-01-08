2fdac460e2106b671f8d5e5260ced93ebcde9b30
yogesh1006 <yogeshkasliwal707@gmail.com>
2023-01-08 22:28:33 +0530

    laying the foundation live class- jsx babel

--- App.js
+++ App.js
@@ -1,47 +1,27 @@
-/*
-* HMR - hot module replacement
-* File watcher algorithm - written in C++
-* Bundling 
-* Minifying
-* Cleaning our code
-* DEV and prod build
-* superfast build algorithm
-* Image optimization
-* Caching while development
-* compression
-* compatible with older versions of browser
-* HTTPS on dev
-* port number chnages automatically if we run more than 1 project simentaneously
-* consistent hashing algorithm
-* zero config
-*
-*
-*
-* Transitive dependencies (dependency tree)- only react and parcel is not make your app performent, 
-* parcel itself also depend or use other packages through it does minification,bundling, compression etc.
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-//creating react element h1
-const heading1 = React.createElement("h1", {
-    className: 'heading1'
-}, "Namaste Javascript!");
+// JSX =>  React.createElement => object => object converted into HTML(DOM) using ReactDOM library
 
 //creating react element h2
-const heading2 = React.createElement("h2", {
-    className: 'heading2'
-}, "Hello");
+const Title = () => (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
+);
 
-//creating react element container-div which contains childrens as heading1 and heading2
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
+const HeaderComponent = () => {
+  return (
+    <div>
+      {Title()}
+      <h2>This is React live Course</h2>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+};
 
 //creating root
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//passing a react element inside root(or in DOM)
-root.render(container);
+//passing a react element or react Functional Component inside root(or in DOM)
+root.render(<HeaderComponent />);
